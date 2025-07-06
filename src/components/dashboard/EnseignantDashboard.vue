584b18f8c3950e2132d7882a20c238e047a3ca14
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-07-06 16:16:23 +0200

    Responsive mobile sur le dashboard enseignant

--- src/components/dashboard/EnseignantDashboard.vue
+++ src/components/dashboard/EnseignantDashboard.vue
@@ -20,13 +20,13 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex flex-row gap-4 w-full">
-    <div class="flex flex-col gap-4 bg-white rounded-lg border border-gray-200 p-4 w-2/3">
+  <div class="flex md:flex-row flex-col gap-4 w-full">
+    <div class="flex flex-col gap-4 bg-white rounded-lg border border-gray-200 p-4 md:w-2/3">
       <div class="font-bold text-xl">Évaluations en cours</div>
       <div class="flex flex-col gap-3">
         <div v-for="evaluation in evaluations" class="p-1 w-full">
-          <div class="flex justify-between items-center">
-            <div class="flex items-center gap-2 max-w-2/3">
+          <div class="flex md:flex-row flex-col justify-between md:items-center">
+            <div class="flex items-center gap-2 md:max-w-2/3">
               <div class="bg-red-100 p-2 rounded-md">
                 <BookOpenIcon class="inline-block size-6 text-red-400" aria-hidden="true" />
               </div>
@@ -36,7 +36,7 @@ onMounted(async () => {
               </div>
             </div>
             <div>
-              <div class="flex items-center gap-2">
+              <div class="flex md:justify-start justify-between items-center gap-2">
                 <div class="text-xs p-1 bg-blue-100 rounded-md text-blue-600 font-bold">
 
               <span v-if="evaluation.date_fin && evaluation.date_debut" class="flex items-center gap-1">
@@ -63,7 +63,7 @@ onMounted(async () => {
       </div>
     </div>
 
-    <div class="flex flex-col gap-4 bg-white rounded-lg border border-gray-200 p-4 w-1/3">
+    <div class="flex flex-col gap-4 bg-white rounded-lg border border-gray-200 p-4 md:w-1/3">
       <div class="font-bold text-xl">Activités récentes</div>
       <div class="flex flex-col">
         <div class="p-1 w-full">
